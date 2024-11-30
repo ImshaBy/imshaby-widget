51f337512945627a6bad59517e1ce3369979f42a
Yan Astrazhynski <87945214+Astrazhynski@users.noreply.github.com>
2024-11-30 19:35:33 +0300

    added days plates

---
+++ src/components/navs/days.tsx
@@ -0,0 +1,36 @@
+import { Prop, Component, h } from "@stencil/core";
+
+@Component({
+    tag: 'day-plate',
+    styleUrl: 'navs.css',
+    shadow: true,
+})
+
+export class DayPlate {
+
+    @Prop() active: string;
+    @Prop() disabled: string;
+    @Prop() k: number;
+    @Prop() aria: string = 'false';
+    @Prop() day: any;
+    @Prop() date: any;
+
+
+    render() {
+        <button
+            class={`nav-link daysNavLink ${this.active} ${this.disabled}`} 
+            id={`v-pills-d${this.k}`} 
+            data-bs-toggle="pill" 
+            data-bs-target={`#v-pills-d${this.k}-tab`} 
+            type="button" role="tab" 
+            aria-controls={`v-pills-d${this.k}-tab`} 
+            aria-selected={`${this.aria}`}>
+            <span class="daysSpan">
+                {this.day}
+                <br />
+                {this.date}
+            </span>
+        </button>
+    }
+
+}
