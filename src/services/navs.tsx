247a416a624f5b13bec0739fb56421923e559e61
Yan Astrazhynski <87945214+Astrazhynski@users.noreply.github.com>
2025-01-04 21:27:32 +0300

    prettier code

--- src/services/navs.tsx
+++ src/services/navs.tsx
@@ -69,22 +69,22 @@ export class DaysNavigation {
         let newDate: Date
 
         if (this.dayInDaysExists(a)) {
-        newDate = new Date( Date.parse( this.scheduleInfo.find(el => el.days == a).date ) )
-        date = newDate.getDate().toString().padStart(2, '0') + '.' + (newDate.getMonth() + 1).toString().padStart(2, '0')
+          newDate = new Date( Date.parse( this.scheduleInfo.find(el => el.days == a).date ) )
+          date = newDate.getDate().toString().padStart(2, '0') + '.' + (newDate.getMonth() + 1).toString().padStart(2, '0')
         } else {
-        disabled = 'disabled'
-        date = '—'
+          disabled = 'disabled'
+          date = '—'
         }
 
         day = D.getWeekName(a)
         if (i == 0) { active = 'active' }
 
         return {
-        active: active,
-        date: date,
-        day: day,
-        dayNumber: a,
-        disabled: disabled,
+          active: active,
+          date: date,
+          day: day,
+          dayNumber: a,
+          disabled: disabled,
         }
     })
 
